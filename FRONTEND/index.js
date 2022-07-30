339e6fb73396b6d565fccc3470fe6fb88634e7b4
Denis Anicai <denisanicai14@gmail.com>
2022-07-30 18:10:57 +0300

    sdf

--- FRONTEND/index.js
+++
@@ -1,3 +0,0 @@
-fetch('https://save-photos.herokuapp.com/')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
--- FRONTEND/react-task-tracker/src/App.js
+++ FRONTEND/react-task-tracker/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from './components/Tasks'
+import {useState} from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: "exam 1",
+        day: "Feb 6th at 1:30 PM",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "exam 2",
+        day: "Feb 8th at 9:30 PM",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "exam 3",
+        day: "Feb 13th at 12:00 PM",
+        reminder: true,
+      }
+    ]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Tasks tasks = {tasks}/>
     </div>
   );
 }
